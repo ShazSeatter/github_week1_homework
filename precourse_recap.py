0ebfc5892f63ae58258cb8bc3285dbc1e620e85d
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-23 20:49:17 +0000

    converted int value to str to concatenate final string

--- precourse_recap.py
+++ precourse_recap.py
@@ -3,7 +3,7 @@ current_year = 2023
 birth_year = 1996
 
 def introduction(name, year, birth):
-    print("Hello " + name + ". You are " + year - birth + " years old.")
+    print("Hello " + name + ". You are " + str(year - birth) + " years old.")
 
 introduction(first_name, current_year, birth_year)
 
